999f7b6f37c0edde6ecfa40b245810b25f89678d
shraddha agnihotri <shraddhaagnihotri529@gmail.com>
2025-09-08 14:52:22 +0530

    first commit

---
+++ logging-middleware/resgister.js
@@ -0,0 +1,26 @@
+async function register() {
+  const response = await fetch("http://20.244.56.144/evaluation-service/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+email: "shraddhaagnihotri729@gmail.com",
+name: "Shraddha Agnihotri",
+mobileNo:"6386472980",
+githubUsername: "ShraddhaAgni3",
+rollNo: "2301641529001",
+accessCode: "sAWTuR"
+}),
+  });
+
+  const text = await response.text(); // get raw response
+  console.log("Raw response:", text);
+
+  try {
+    const data = JSON.parse(text);
+    console.log("Parsed JSON:", data);
+  } catch (err) {
+    console.error("Response is not JSON, maybe an error page.");
+  }
+}
+
+register();
